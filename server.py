0c312bf2007ba4cf55aac70660d7a83aaf727d79
Hariprasth M <hphari687@gmail.com>
2021-03-13 13:04:32 +0530

    added registerartion

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
-
+# Routes for our page comes here #
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
+# Routes for sending Favicon #
 @app.route('/favicon.ico')
 def favicon():
 	return send_from_directory("static/favicons", "favicon.ico")

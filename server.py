c9a53f2adda68d1c8adc99f0dde1f2fa67d05181
Hariprasth M <hphari687@gmail.com>
2021-03-13 13:56:45 +0530

    added rawdata

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/register')
+def register():
+	return render_template("register.html")
 
 # Routes for sending Favicon #
 @app.route('/favicon.ico')

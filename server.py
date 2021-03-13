a4dc4fefc0f0224d21595b19bf4e202fd980be6f
Hariprasth M <hphari687@gmail.com>
2021-03-13 12:51:32 +0530

    added missing files

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory("static/favicons", "favicon.ico")
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8000, debug=True)

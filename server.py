7fb3c75afae849ed09b6a2d1528dd6d00306e500
MageshBabu K <mage1k99@gmail.com>
2021-03-14 13:43:16 +0530

    seperate route for getting data

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, jsonify
 import cx_Oracle
+import json
 app = Flask(__name__)
 
-connection = cx_Oracle.connect("project","project")
+# Setting the json data from file
+jsonfile = ""
+with open("rawdata.json", "r", encoding="utf-8") as js:
+	jsonfile = json.load(js)
+
+# connection = cx_Oracle.connect("project","project")
 
 # Routes for our page comes here #
 @app.route('/')
@@ -11,15 +17,27 @@ def index():
 
 @app.route('/register')
 def register():
-	return render_template("hehe.html")
+	return render_template("register.html")
 
-@app.route('/test')
-def test():
-	cursor = connection.cursor()
-	cursor.execute("CREATE TABLE testdb(firstname varchar(255),lastname varchar(255),phoneNumber number(12))")
-	a = cursor.execute("DESCRIBE TestDB")
-	print(a)
-	return connection.version
+# @app.route('/test')
+# def test():
+# 	cursor = connection.cursor()
+# 	cursor.execute("CREATE TABLE testdb(firstname varchar(255),lastname varchar(255),phoneNumber number(12))")
+# 	a = cursor.execute("DESCRIBE TestDB")
+# 	print(a)
+# 	return connection.version
+
+@app.route('/liststates', methods=['POST'])
+def getstates():
+	listofstates = []
+	for data in jsonfile["states"]:
+		listofstates.append(
+			{
+				"state": data["name"],
+				"districts": data["districts"]
+			}
+		)
+	return jsonify({"result": listofstates})
 
 # Routes for sending Favicon #
 @app.route('/favicon.ico')

ed8464edfdd03316fbed34e53fcb81e21a8024d2
MageshBabu K <mage1k99@gmail.com>
2021-03-15 01:25:09 +0530

    added login from kesav

--- server.py
+++ server.py
@@ -20,6 +20,10 @@ def index():
 def register():
 	return render_template("register.html")
 
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
 # @app.route('/test')
 # def test():
 # 	cursor = connection.cursor()

d9c844c827a670ea62be357e82905dc77bc68c97
MageshBabu K <mage1k99@gmail.com>
2021-03-22 00:20:38 +0530

    updated with father name

--- ultihasher.py
+++ ultihasher.py
@@ -49,7 +49,7 @@ def generateAadhar():
 		encrypted_pincode = AESCipher.encrypt(str(data["PINCODE"]), secret_pwd)
 		encrypted_isEligible = AESCipher.encrypt(data["isEligible"], secret_pwd)
 		# 
-		oracle_writer.write("INSERT INTO AADHAAR_DEMO (AADHAAR_NO, FIRST_NAME, LAST_NAME, GENDER, MOBILE_NO, STATE, DISTRICT, SUB_DISTRICT, BLOCK, VILLAGE, PINCODE, DOB, Eligiblity) VALUES")
+		oracle_writer.write("INSERT INTO AADHAAR_DEMO (AADHAAR_NO, FIRST_NAME, LAST_NAME, FATHERNAME GENDER, MOBILE_NO, STATE, DISTRICT, SUB_DISTRICT, BLOCK, VILLAGE, PINCODE, DOB, Eligiblity) VALUES")
 		oracle_writer.write("('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'"
 			.format(
 					encrypted_aadhaar,

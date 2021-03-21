54af2efcccde66c1bfdf4dbd3a2518a0d5d951c0
MageshBabu K <mage1k99@gmail.com>
2021-03-21 23:35:52 +0530

    fix erros on creation

--- ultihasher.py
+++ ultihasher.py
@@ -50,7 +50,7 @@ def generateAadhar():
 		encrypted_isEligible = AESCipher.encrypt(data["isEligible"], secret_pwd)
 		# 
 		oracle_writer.write("INSERT INTO AADHAAR_DEMO (AADHAAR_NO, FIRST_NAME, LAST_NAME, GENDER, MOBILE_NO, STATE, DISTRICT, SUB_DISTRICT, BLOCK, VILLAGE, PINCODE, DOB, Eligiblity) VALUES")
-		oracle_writer.write("({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}"
+		oracle_writer.write("('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'"
 			.format(
 					encrypted_aadhaar,
 					encrypted_first_name,
